6b5f1db2011bd2395b4463f49f44003f14e18254
matthewamaiale <51253950+matthewamaiale@users.noreply.github.com>
2020-07-25 17:06:46 -0400

    Update typography.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,16 +3,16 @@ import Wordpress2016 from "typography-theme-wordpress-2016"
 
 Wordpress2016.googleFonts = [
   {
-    name: "Source Sans Pro",
+    name: "Inter",
     styles: ["400", "400i", "700", "700i", "900", "900i"],
   },
 ]
-Wordpress2016.headerFontFamily = ["Source Sans Pro"]
-Wordpress2016.bodyFontFamily = ["Source Sans Pro"]
+Wordpress2016.headerFontFamily = ["Inter"]
+Wordpress2016.bodyFontFamily = ["Inter"]
 Wordpress2016.overrideThemeStyles = () => {
   return {
     h1: {
-      fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+      fontFamily: ["Inter", "sans-serif"].join(","),
     },
     "a.gatsby-resp-image-link": {
       boxShadow: `none`,

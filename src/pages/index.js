8b7e90dd34bb07a54c09e4b29b4f721a2946f4ac
matthewamaiale <51253950+matthewamaiale@users.noreply.github.com>
2020-07-27 23:48:48 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
-const { author, social } = data.site.siteMetadata
-  return (
-    <div
-      style={{
-        display: `flex`,
-        flexWrap: `wrap`,
-        marginBottom: rhythm(2.5),
-      }}
-    >
-
-      <p className="hidden" id="pp2">
-        Hey, I'm Matthew. I'm a second-year studying Finance and Economics at{" "}
-        <Link
-          href="https://psu.edu/"
-          target="_blank"
-        >
-          Penn State
-        </Link>
-        . I'm currently taking a gap semester to co-op in tech and venture capital in <Link className="highlight1" href="https://www.youtube.com/watch?v=8RZqPq1-1Tw" target="_blank">San Francisco</Link>. I'm passionate about quantitative analysis, technology, and their <Link href="https://medium.com/@maiale/the-foes-and-fallacies-of-bitcoin-and-why-millennials-arent-ready-to-invest-9a096eb52af6" target="_blank" className="highlight1">unique intersection</Link>. I also love <Link href="https://docs.google.com/document/d/1GrIVKaveYCPvnJQSQQhxn0pXMeWN-j4DtpuXFBtGqoI/edit?usp=sharing" target="_blank" className="highlight1">concerts</Link>, spontaneous excursions, <Link className="highlight1" href="https://medium.com/@maiale/intrinsic-motivation-of-running-512a4c97cde2" target="_blank">psychology</Link>, and <Link href="https://docs.google.com/document/d/1sYKeHzy3oulGrTv7__blwa6JnNiDRIzvCfDqSsVQBRM/edit?usp=sharing" target="_blank" className="highlight1">meeting new people</Link>.
-      </p>
-          }}
-
-
-   
-    </div>
-  )
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"

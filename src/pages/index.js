f10ba6ebb524984d16e08325bb3ef63bfa0dffa9
matthewamaiale <51253950+matthewamaiale@users.noreply.github.com>
2020-07-28 00:08:31 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 
-import Bio from "../components/bio"
+import Top from "../components/top.js"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
@@ -16,7 +16,7 @@ class BlogIndex extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
-    
+    < Top />
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return (

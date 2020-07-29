99b8b6d45719b0141534779d2dfeca51e7510409
matthewamaiale <51253950+matthewamaiale@users.noreply.github.com>
2020-07-29 07:38:56 -0400

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -54,7 +54,11 @@ const Bio = () => {
         marginBottom: rhythm(2.5),
       }}
     >
-	    <b>What I'm up to </b>
+	    <b
+		style=({
+			fontSize: `1.5em`,
+		})
+		>What I'm up to </b>
       <p className="hidden" id="pp2">
        I took a gap semester in spring 2020 to work at a venture capital fund in San Francisco. After working with tons of awesome investors and founders, I decided to double down and also take off the fall semester to join <Link className="highlight1" href="https://demandcurve.com" target="_blank">Demand Curve (YC S19)</Link>.
 	      </p>

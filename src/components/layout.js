b475b7cd75a48b727890244c232d5b746b1e93cd
matthewamaiale <51253950+matthewamaiale@users.noreply.github.com>
2020-08-15 10:59:04 -0400

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,22 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import { rhythm, scale } from "../utils/typography"
-const Link = styled.a`
-  background: linear-gradient(181deg,rgba(255,255,255,0) 60%, rgb(225,208,255,.6) 40%);
-  border-radius: 17%;
-  border-bottom: dotted 0px;
-  transition: color 0.2s ease-in-out, border-color 0.2s ease-in-out;
-  text-decoration: none;
-  box-shadow: none;
-  &:hover {
-    background: linear-gradient(180deg,rgba(255,255,255,0) 60%, rgba(0,87,169, .13) 40%);
-    border-radius: 17%;
-    border-bottom: dotted 0px;
-    text-decoration: none;
-    box-shadow: none;
-    color: #8f7afb;
-  }
-`
 
 class Layout extends React.Component {
   render() {
@@ -79,7 +63,23 @@ class Layout extends React.Component {
         <header>{header}</header>
         <main>{children}</main>
         <footer>
-         <Link class="highlight1" href="mailto:m@maiale.me" target="_blank">Email</Link> ∙ <Link class="highlight1" href="https://www.linkedin.com/in/mattmaiale/" target="_blank">LinkedIn</Link> ∙ <Link href="https://medium.com/@maiale" target="_blank" class="highlight1">Medium</Link> ∙ 
+         <Link 
+        style={{
+               background: linear-gradient(181deg,rgba(255,255,255,0) 60%, rgb(225,208,255,.6) 40%);
+  border-radius: 17%;
+  border-bottom: dotted 0px;
+  transition: color 0.2s ease-in-out, border-color 0.2s ease-in-out;
+  text-decoration: none;
+  box-shadow: none;
+  &:hover {
+    background: linear-gradient(180deg,rgba(255,255,255,0) 60%, rgba(0,87,169, .13) 40%);
+    border-radius: 17%;
+    border-bottom: dotted 0px;
+    text-decoration: none;
+    box-shadow: none;
+    color: #8f7afb;
+              }}
+ href="mailto:m@maiale.me" target="_blank">Email</Link> ∙ <Link class="highlight1" href="https://www.linkedin.com/in/mattmaiale/" target="_blank">LinkedIn</Link> ∙ <Link href="https://medium.com/@maiale" target="_blank" class="highlight1">Medium</Link> ∙ 
 <Link class="highlight1" href="https://www.twitter.com/mattmaiale" target="_blank">Twitter</Link>
         </footer>
       </div>

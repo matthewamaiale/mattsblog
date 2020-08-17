3e0ba2a02cea946907f563fd2f475052b30a4a88
matthewamaiale <51253950+matthewamaiale@users.noreply.github.com>
2020-08-17 13:23:12 -0400

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -66,7 +66,7 @@ class Layout extends React.Component {
       >
         <header>{header}</header>
         <main>{children}</main>
-        <footer><p>More on me: 
+        <footer><p> 
 <a style={{
               boxShadow: `none`,
               textDecoration: `none`,
